2b0ae180e4852a9213d8d58b48da820dcb3936a5
whoisonezero <whoisonezero@DESKTOP-E2SDJGT>
2024-10-02 14:22:31 +0000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnStart = New Button()
+        varButton = New Button()
+        SuspendLayout()
+        ' 
+        ' btnStart
+        ' 
+        btnStart.Location = New Point(37, 51)
+        btnStart.Name = "btnStart"
+        btnStart.Size = New Size(227, 116)
+        btnStart.TabIndex = 0
+        btnStart.Text = "Press Here"
+        btnStart.UseVisualStyleBackColor = True
+        ' 
+        ' varButton
+        ' 
+        varButton.Location = New Point(333, 51)
+        varButton.Name = "varButton"
+        varButton.Size = New Size(210, 120)
+        varButton.TabIndex = 1
+        varButton.Text = "Variable Demo"
+        varButton.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(varButton)
+        Controls.Add(btnStart)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btnStart As Button
+    Friend WithEvents varButton As Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        MsgBox("Hello World!")
+        MessageBox.Show("another message")
+    End Sub
+
+    Private Sub varButton_Click(sender As Object, e As EventArgs) Handles varButton.Click
+        Dim firstName As String
+        Dim age As Integer
+        Dim isMale As Boolean
+        Dim height As Double
+        Dim weight As Decimal
+        Dim dateOfBirth As Date
+
+        firstName = "Shalom"
+
+        MessageBox.Show("Hello " & firstName)
+
+        age = 45
+        isMale = True
+        height = 144.34
+        weight = 55
+        dateOfBirth = #09/03/2000#
+
+    End Sub
+End Class
