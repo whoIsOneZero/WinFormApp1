792631eb2cc488ab8bac32852c73af2922a71f87
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-10-02 16:41:20 +0000

    textBoxes and listBoxes

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,39 +24,133 @@ Partial Class Form1
     Private Sub InitializeComponent()
         btnStart = New Button()
         varButton = New Button()
+        userInput = New Button()
+        txtUsername = New TextBox()
+        nameLabel = New Label()
+        txtLastName = New TextBox()
+        lastNameLabel = New Label()
+        SubmitButton = New Button()
+        lstOccupation = New ListBox()
+        lblOccupation = New Label()
         SuspendLayout()
         ' 
         ' btnStart
         ' 
-        btnStart.Location = New Point(37, 51)
+        btnStart.Location = New Point(37, 108)
         btnStart.Name = "btnStart"
-        btnStart.Size = New Size(227, 116)
+        btnStart.Size = New Size(109, 81)
         btnStart.TabIndex = 0
         btnStart.Text = "Press Here"
         btnStart.UseVisualStyleBackColor = True
         ' 
         ' varButton
         ' 
-        varButton.Location = New Point(333, 51)
+        varButton.Location = New Point(37, -9)
         varButton.Name = "varButton"
-        varButton.Size = New Size(210, 120)
+        varButton.Size = New Size(102, 100)
         varButton.TabIndex = 1
         varButton.Text = "Variable Demo"
         varButton.UseVisualStyleBackColor = True
+        ' 
+        ' userInput
+        ' 
+        userInput.Location = New Point(42, 207)
+        userInput.Name = "userInput"
+        userInput.Size = New Size(104, 77)
+        userInput.TabIndex = 2
+        userInput.Text = "Enter Name"
+        userInput.UseVisualStyleBackColor = True
+        ' 
+        ' txtUsername
+        ' 
+        txtUsername.Location = New Point(259, 54)
+        txtUsername.Name = "txtUsername"
+        txtUsername.Size = New Size(100, 23)
+        txtUsername.TabIndex = 3
+        ' 
+        ' nameLabel
+        ' 
+        nameLabel.AutoSize = True
+        nameLabel.Location = New Point(262, 34)
+        nameLabel.Name = "nameLabel"
+        nameLabel.Size = New Size(60, 15)
+        nameLabel.TabIndex = 4
+        nameLabel.Text = "Username"
+        ' 
+        ' txtLastName
+        ' 
+        txtLastName.Location = New Point(259, 104)
+        txtLastName.Name = "txtLastName"
+        txtLastName.Size = New Size(100, 23)
+        txtLastName.TabIndex = 5
+        ' 
+        ' lastNameLabel
+        ' 
+        lastNameLabel.AutoSize = True
+        lastNameLabel.Location = New Point(262, 86)
+        lastNameLabel.Name = "lastNameLabel"
+        lastNameLabel.Size = New Size(63, 15)
+        lastNameLabel.TabIndex = 6
+        lastNameLabel.Text = "Last Name"
+        ' 
+        ' SubmitButton
+        ' 
+        SubmitButton.Location = New Point(274, 154)
+        SubmitButton.Name = "SubmitButton"
+        SubmitButton.Size = New Size(75, 23)
+        SubmitButton.TabIndex = 7
+        SubmitButton.Text = "Submit"
+        SubmitButton.UseVisualStyleBackColor = True
+        ' 
+        ' lstOccupation
+        ' 
+        lstOccupation.FormattingEnabled = True
+        lstOccupation.ItemHeight = 15
+        lstOccupation.Items.AddRange(New Object() {"Doctor", "Programmer", "Teacher", "Student", "Tinker", "Tailor", "Soldier", "Sailor"})
+        lstOccupation.Location = New Point(510, 89)
+        lstOccupation.Name = "lstOccupation"
+        lstOccupation.Size = New Size(131, 124)
+        lstOccupation.TabIndex = 8
+        ' 
+        ' lblOccupation
+        ' 
+        lblOccupation.AutoSize = True
+        lblOccupation.Location = New Point(510, 71)
+        lblOccupation.Name = "lblOccupation"
+        lblOccupation.Size = New Size(69, 15)
+        lblOccupation.TabIndex = 9
+        lblOccupation.Text = "Occupation"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(lblOccupation)
+        Controls.Add(lstOccupation)
+        Controls.Add(SubmitButton)
+        Controls.Add(lastNameLabel)
+        Controls.Add(txtLastName)
+        Controls.Add(nameLabel)
+        Controls.Add(txtUsername)
+        Controls.Add(userInput)
         Controls.Add(varButton)
         Controls.Add(btnStart)
         Name = "Form1"
         Text = "Form1"
         ResumeLayout(False)
+        PerformLayout()
     End Sub
 
     Friend WithEvents btnStart As Button
     Friend WithEvents varButton As Button
+    Friend WithEvents userInput As Button
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents nameLabel As Label
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents lastNameLabel As Label
+    Friend WithEvents SubmitButton As Button
+    Friend WithEvents lstOccupation As ListBox
+    Friend WithEvents lblOccupation As Label
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -27,4 +27,29 @@
                         " that you are male" & vbCrLf & "Yes!")
 
     End Sub
+
+    Private Sub userInput_Click(sender As Object, e As EventArgs) Handles userInput.Click
+        Dim userName As String
+
+        userName = InputBox("Please enter your name: ")
+
+        MessageBox.Show("Hello, " & userName)
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles SubmitButton.Click
+        Dim stUserName As String
+        Dim stLastName As String
+        Dim stOccupation As String
+
+        stUserName = txtUsername.Text
+        stLastName = txtLastName.Text
+
+        stOccupation = lstOccupation.SelectedItem
+
+        MessageBox.Show("Hello, " & stUserName & ". You are Mr. " & stLastName &
+                        ", a " & stOccupation)
+
+
+    End Sub
 End Class
